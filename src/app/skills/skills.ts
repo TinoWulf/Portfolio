556662407eb4a089b4dea190a93424be3d8192f2
TinoWulf <wulf.tino.gz@gmail.com>
2026-01-02 11:30:14 +0100

    The skills section was edited with TypeScript to allow skills to be rendered dynamically.

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { SkillBox } from './skill-box/skill-box';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [SkillBox],
+  standalone: true,
+  imports: [SkillBox, CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
 export class Skills {
-
+  skills = [
+    { name: 'HTML', src: 'html' },
+    { name: 'CSS', src: 'css' },
+    { name: 'JavaScript', src: 'javascript' },
+    { name: 'Material Design', src: 'material-design' },
+    { name: 'TypeScript', src: 'typescript' },
+    { name: 'Angular', src: 'angular' },
+    { name: 'Firebase', src: 'firebase' },
+    { name: 'Git', src: 'git-skill-icon' },
+    { name: 'REST-API', src: 'rest-api' },
+    { name: 'Scrum', src: 'scrum' },
+    { name: 'Growth Mindset', src: 'mindset' },
+  ]
 }
