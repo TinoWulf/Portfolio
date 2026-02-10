a23e60b0f29c05260a6b9839e4de72effbb4ac6c
TinoWulf <wulf.tino.gz@gmail.com>
2026-02-10 13:06:50 +0100

    PC changes before pulling laptop updates

--- src/app/contact-me.component/contact-sheet.component/contact-sheet.component.ts
+++ src/app/contact-me.component/contact-sheet.component/contact-sheet.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { TranslationService } from '../../services/translation.services';
+import { CommonModule } from '@angular/common';
+import { log } from 'console';
 
 @Component({
   selector: 'app-contact-sheet',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact-sheet.component.html',
   styleUrl: './contact-sheet.component.scss',
 })
 export class ContactSheet {
+  isChecked = false;
+  triedSubmit = false;
+  
   constructor(public translation: TranslationService) {}
+
+  onCheckboxChange (event: Event) {
+    this.isChecked = (event.target as HTMLInputElement).checked;
+    if (this.isChecked) {
+      this.triedSubmit = false;
+    }
+  }
+
+  onSubmitClick() {
+    if (!this.isChecked) {
+      this.triedSubmit = true;      
+      return;
+    }
+    this.triedSubmit = false;
+    console.log('Form gesendet');
+  }
 }
