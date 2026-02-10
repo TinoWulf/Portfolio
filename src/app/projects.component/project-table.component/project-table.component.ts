bd627d9a9738fe9afff9d993d8192adc1e424ae8
TinoWulf <wulf.tino.gz@gmail.com>
2026-02-10 12:52:35 +0100

    transfer

--- src/app/projects.component/project-table.component/project-table.component.ts
+++ src/app/projects.component/project-table.component/project-table.component.ts
@@ -9,28 +9,45 @@ import { Component } from '@angular/core';
   styleUrl: './project-table.component.scss',
 })
 export class ProjectTable {
-  
+
   projects = [
     {
       id: 1,
       key: 'join',
       title: 'Join',
       image: '/assets/img/screenshots/join-portfolio.png',
-      techs: ['JavaScript', 'HTML', 'CSS', 'Firebase']
+      techs: [
+        { name: 'JavaScript', icon: '/assets/img/icons/turquoise-icon-javascript.png' },
+        { name: 'HTML', icon: '/assets/img/icons/turquoise-icon-html.png' },
+        { name: 'CSS', icon: '/assets/img/icons/turquoise-icon-css.png' },
+        { name: 'Firebase', icon: '/assets/img/icons/turquoise-icon-firebase.png' }
+      ],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
     },
     {
       id: 2,
       key: 'sharkie',
       title: 'Sharkie',
       image: '/assets/img/screenshots/sharkie-portfolio.png',
-      techs: ['JavaScript', 'HTML', 'CSS']
+      techs: [
+        { name: 'JavaScript', icon: '/assets/img/icons/turquoise-icon-javascript.png' },
+        { name: 'HTML', icon: '/assets/img/icons/turquoise-icon-html.png' },
+        { name: 'CSS', icon: '/assets/img/icons/turquoise-icon-css.png' }
+      ],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
     },
     {
       id: 3,
       key: 'pokedex',
       title: 'Pokedex',
       image: '/assets/img/screenshots/pokedex-portfolio.png',
-      techs: ['JavaScript', 'HTML', 'CSS', 'REST-API']
+      techs: [
+        { name: 'JavaScript', icon: '/assets/img/icons/turquoise-icon-javascript.png' },
+        { name: 'HTML', icon: '/assets/img/icons/turquoise-icon-html.png' },
+        { name: 'CSS', icon: '/assets/img/icons/turquoise-icon-css.png' },
+        { name: 'REST-API', icon: '/assets/img/icons/turquoise-icon-rest-api.png' }
+      ],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.'
     }
   ];
 
